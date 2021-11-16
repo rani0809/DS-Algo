300c2eb0fe0c4fd28c076601d197ce836fe39f4f
Rani <ranidhage007@gmail.com>
2021-11-16 22:57:39 +0530

    Time: 60 ms (89.70%), Space: 27.4 MB (87.67%) - LeetHub

---
+++ 01-matrix/01-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution { // 48 ms, faster than 99.64%
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+        int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r - 1 >= 0) top = mat[r - 1][c];
+                if (c - 1 >= 0) left = mat[r][c - 1];
+                mat[r][c] = min(top, left) + 1;
+            }
+        }
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r + 1 < m) bottom = mat[r + 1][c];
+                if (c + 1 < n) right = mat[r][c + 1];
+                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
+            }
+        }
+        return mat;
+    }
+};
