0b1cd79d3f91a6303be66ca1fdac27382d230a06
Rani <ranidhage007@gmail.com>
2021-11-19 23:34:31 +0530

    Time: 4 ms (35.15%), Space: 7.9 MB (21.59%) - LeetHub

---
+++ house-robber/house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        const int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+        vector<int> f(n, 0);
+        f[0] = nums[0];
+        f[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++i)
+            f[i] = max(f[i-2] + nums[i], f[i-1]);
+        return f[n-1];
+    }
+};
