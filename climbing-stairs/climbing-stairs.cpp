18fe9fa6f870d11410291671f7c19808dd3c23f6
Rani <ranidhage007@gmail.com>
2021-11-19 23:31:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (26.14%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if ( n == 0 ) return 0;
+        else if ( n == 1 ) return 1;
+        else if ( n == 2 ) return 2;
+        
+        vector<int> ways(n+1, 0);
+        ways[1] = 1; ways[2] = 2;
+        for ( int i = 3; i <= n; i++ ) {
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+        
+        return ways[n];
+    }
+};
