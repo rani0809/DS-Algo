ad276c6785778b9cae6295aea7baf6b43cb40e1b
Rani <ranidhage007@gmail.com>
2021-12-09 23:51:54 +0530

    Time: 40 ms (80.43%), Space: 36.3 MB (22.79%) - LeetHub

---
+++ jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+
+public:
+    
+    bool ff(vector<int>& arr, vector<bool>& visited, int i) {
+        if (i < 0 || i >= arr.size() || visited[i]) return false;
+        if (arr[i] == 0) return true;
+        visited[i] = true;
+        return ff(arr, visited, i + arr[i]) || ff(arr, visited, i - arr[i]);
+    }
+    
+    bool canReach(vector<int>& arr, int start) {
+        vector<bool> visited(arr.size(), false);
+        return ff(arr, visited, start);
+    }
+};
