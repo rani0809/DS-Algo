e7d852323038a84f314caf1aa09be1d60d50665c
Rani <ranidhage007@gmail.com>
2021-12-21 00:27:49 +0530

    Time: 8 ms (97.90%), Space: 13.2 MB (52.94%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int R = matrix.size();
-        int C = matrix[0].size();
-        set<int> rows ;
-        set<int> cols ;
-        // We mark the rows and columns that are to be made zero
-        for (int i = 0; i < R; i++) 
+    void setZeroes(vector<vector<int> > &matrix) {
+        
+        int H = matrix.size();
+        int W = matrix[0].size();
+        
+        // find the last 0 row
+        int last_0_row = -1;
+        for (int y = H - 1; y >= 0 && last_0_row == -1; y--)
+            for (int x = 0; x < W; x++)
+                if (matrix[y][x] == 0)
+                {
+                    last_0_row = y;
+                    break;
+                }
+        if (last_0_row == -1)
+            return;
+        
+        // go row by row
+        for (int y = 0; y < last_0_row; y++)
         {
-          for (int j = 0; j < C; j++) 
-          {
-            if (matrix[i][j] == 0) 
+            bool this_is_a_0_row = false;
+            
+            for (int x = 0; x < W; x++)
             {
-                rows.insert(i);
-                cols.insert(j);
+                if (matrix[y][x] == 0)
+                {
+                    this_is_a_0_row = true;
+                    matrix[last_0_row][x] = 0;
+                }
             }
-          }
+            
+            if (this_is_a_0_row)
+            for (int x = 0; x < W; x++)
+            {
+                matrix[y][x] = 0;
+            }
         }
-        // Iterate over the array once again and using the rows and cols sets, update the elements.
-        for (int i = 0; i < R; i++) 
+        
+        // set collums to 0
+        for (int y = 0; y < H; y++)
+        for (int x = 0; x < W; x++)
         {
-          for (int j = 0; j < C; j++) 
-          {
-            if (rows.count(i) || cols.count(j)) 
-            {
-              matrix[i][j] = 0;
-            }
-          }
+            if (matrix[last_0_row][x] == 0)
+                matrix[y][x] = 0;
+        }
+        
+        // set the last 0 row 
+        for (int x = 0; x < W; x++)
+        {
+            matrix[last_0_row][x] = 0;
         }
     }
 };
