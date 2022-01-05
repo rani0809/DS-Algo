0c2257fd987494badc620fa0e09cbe8220790e6c
Rani <ranidhage007@gmail.com>
2022-01-06 00:34:55 +0530

    Time: 91 ms (32.50%), Space: 48.2 MB (94.60%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+    int size=nums.size();
+    int step=nums[0];
+    for(int i=1;i<size;++i){
+        step--;
+        if(step<0)
+           return false;
+        if(nums[i]>step)
+           step=nums[i];
+    }
+    return 1;
+}
+};
