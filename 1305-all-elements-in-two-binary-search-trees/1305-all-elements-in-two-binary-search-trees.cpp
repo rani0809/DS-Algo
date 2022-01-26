55c75a88f50f49034a642eb23055e8a13fcb3d41
Rani <ranidhage007@gmail.com>
2022-01-26 16:30:33 +0530

    Time: 317 ms (5.50%), Space: 85.3 MB (48.31%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -9,34 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        stack<TreeNode *> st1, st2;
-        vector<int> res;
-        
-        while(root1 || root2 || !st1.empty() || !st2.empty()){
-            while(root1){
-                st1.push(root1);
-                root1 = root1->left;
-            }
-            while(root2){
-                st2.push(root2);
-                root2 = root2->left;
-            }
-            if(st2.empty() || (!st1.empty() && st1.top()->val <= st2.top()->val)){
-                root1 = st1.top();
-                st1.pop();
-                res.push_back(root1->val);
-                root1 = root1->right;
-            }
-            else{
-                root2 = st2.top();
-                st2.pop();
-                res.push_back(root2->val);
-                root2 = root2->right;
-            }
-        }
-        return res;
-    }
+     vector<int> res;
+     void dfs(TreeNode *root)
+     {
+          if (!root)
+               return;
+          res.push_back(root->val);
+          dfs(root->left);
+          dfs(root->right);
+     }
+     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+     {
+          dfs(root1);
+          dfs(root2);
+          sort(begin(res), end(res));
+          return res;
+     }
 };
