ea3cb0b66437d57b599286230337fcd1ee055f63
Rani <ranidhage007@gmail.com>
2022-08-01 23:56:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (59.42%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> pre(n, 1), cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] = pre[j] + cur[j - 1];
+            }
+            swap(pre, cur);
+        }
+        return pre[n - 1];
+    }
+};
