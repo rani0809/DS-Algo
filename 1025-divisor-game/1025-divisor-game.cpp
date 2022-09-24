f962ea354330f5ef3be17b0db45b9ca9d7f4a048
Rani <ranidhage007@gmail.com>
2022-09-24 15:35:11 +0530

    Time: 12 ms (5.61%), Space: 5.9 MB (70.45%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+      
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=2;i<n+1;i++){
+            for(int j=1;j<i;j++){
+                if(i%j==0)dp[i] = dp[i] | !dp[i-j];  //(!dp[i-j] checks if i-j is a losing position)
+            }
+        }return dp[n];
+    }  
+    
+};
